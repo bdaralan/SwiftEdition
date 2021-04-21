6f42c279a62aff855f23ea4581d0800795b10056
Dara <bdaralan@gmail.com>
2021-04-21 15:28:56 -0700

    Update Render init - add else condition support

--- Sources/InterfaceEdition/View/Render.swift
+++ Sources/InterfaceEdition/View/Render.swift
@@ -1,39 +1,62 @@
 import SwiftUI
 
 
-public struct Render<Content, Value>: View where Content: View {
+public struct Render<ContentA, ContentB, Value>: View where ContentA: View, ContentB: View {
     
-    private let ifContent: (() -> Content)?
+    private let ifContent: (() -> ContentA)?
     private let ifContentCondition: Bool
     
-    private let mapContent: ((Value) -> Content)?
+    private let mapContent: ((Value) -> ContentA)?
     private let mapContentValue: Value?
     
-    public init(mapped value: Value?, @ViewBuilder content: @escaping (Value) -> Content) {
-        mapContent = content
-        mapContentValue = value
-        ifContent = nil
-        ifContentCondition = false
-    }
+    private let contentB: (() -> ContentB)?
     
     public var body: some View {
         if ifContentCondition, let content = ifContent {
             content()
-        }
-        if let value = mapContentValue, let content = mapContent {
+        } else if let contentB = contentB {
+            contentB()
+        } else if let value = mapContentValue, let content = mapContent {
             content(value)
+        } else if let contentB = contentB {
+            contentB()
         }
     }
 }
 
 
-extension Render where Value == Never {
+extension Render {
     
-    public init(if condition: Bool, @ViewBuilder content: @escaping () -> Content) {
+    public init(if condition: Value, @ViewBuilder content: @escaping () -> ContentA, @ViewBuilder else contentB: @escaping () -> ContentB) where Value == Bool {
         ifContentCondition = condition
         ifContent = content
-        mapContent = nil
         mapContentValue = nil
+        mapContent = nil
+        self.contentB = contentB
+    }
+    
+    public init(if condition: Bool, @ViewBuilder content: @escaping () -> ContentA) where Value == Bool, ContentB == Never {
+        ifContentCondition = condition
+        ifContent = content
+        mapContentValue = nil
+        mapContent = nil
+        contentB = nil
+    }
+    
+    public init(mapped value: Value?, @ViewBuilder content: @escaping (Value) -> ContentA, @ViewBuilder else contentB: @escaping () -> ContentB) {
+        mapContentValue = value
+        mapContent = content
+        ifContentCondition = false
+        ifContent = nil
+        self.contentB = contentB
+    }
+    
+    public init(mapped value: Value?, @ViewBuilder content: @escaping (Value) -> ContentA) where ContentB == Never {
+        mapContentValue = value
+        mapContent = content
+        ifContentCondition = false
+        ifContent = nil
+        contentB = nil
     }
 }
 
@@ -57,6 +80,20 @@ struct Render_Previews: PreviewProvider {
         
         Render(mapped: Int("4")) { value in
             Text("Render(mapped: Int('4'))")
+        }
+        .previewLayout(.sizeThatFits)
+        
+        Render(if: false) {
+            Text("Render(if: true)")
+        } else: {
+            Text("Render(if: false)")
+        }
+        .previewLayout(.sizeThatFits)
+        
+        Render(mapped: Int("nil")) { value in
+            Text("THIS SHOULD NOT BE RENDED")
+        } else: {
+            Text("mapped nil")
         }
         .previewLayout(.sizeThatFits)
     }
