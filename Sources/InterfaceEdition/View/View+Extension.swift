2ef63025fdf2884c31bf328904dc63c69abbf7f4
Dara <bdaralan@gmail.com>
2021-05-09 10:05:15 -0700

    Make View's onReceive & onChange public

--- Sources/InterfaceEdition/View/View+Extension.swift
+++ Sources/InterfaceEdition/View/View+Extension.swift
@@ -4,11 +4,11 @@ import Combine
 
 extension View {
     
-    func onReceive<P>(_ publisher: P, assignTo binding: Binding<P.Output>) -> some View where P: Publisher, P.Failure == Never {
+    public func onReceive<P>(_ publisher: P, assignTo binding: Binding<P.Output>) -> some View where P: Publisher, P.Failure == Never {
         onReceive(publisher, perform: { binding.wrappedValue = $0 })
     }
     
-    func onChange<Value>(of value: Value, assignTo binding: Binding<Value>) -> some View where Value: Equatable {
+    public func onChange<Value>(_ value: Value, assignTo binding: Binding<Value>) -> some View where Value: Equatable {
         onChange(of: value, perform: { binding.wrappedValue = $0 })
     }
 }
