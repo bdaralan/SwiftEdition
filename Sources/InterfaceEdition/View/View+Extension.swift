b20cfaa3fddf6e66e74cb966dde86441698f25c2
Dara <bdaralan@gmail.com>
2021-05-09 07:05:02 -0700

    Add onChange(of:assignTo:)

--- Sources/InterfaceEdition/View/View+Extension.swift
+++ Sources/InterfaceEdition/View/View+Extension.swift
@@ -7,4 +7,8 @@ extension View {
     func onReceive<P>(_ publisher: P, assignTo binding: Binding<P.Output>) -> some View where P: Publisher, P.Failure == Never {
         onReceive(publisher, perform: { binding.wrappedValue = $0 })
     }
+    
+    func onChange<Value>(of value: Value, assignTo binding: Binding<Value>) -> some View where Value: Equatable {
+        onChange(of: value, perform: { binding.wrappedValue = $0 })
+    }
 }
